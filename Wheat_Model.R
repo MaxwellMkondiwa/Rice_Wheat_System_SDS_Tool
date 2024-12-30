1fe0d8f085486933fe2a379ec7f5dbc81432dbae
MaxwellMkondiwa <mkond001@umn.edu>
2024-12-30 20:16:43 +0530

    update

--- Wheat_Model.R
+++ Wheat_Model.R
@@ -1,5 +1,54 @@
 # Wheat model
 
+rm(list = ls())
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(sp, mgcv, bamlss,BayesX,R2BayesX,sf,spdep,rio,distreg.vis)
+
+setwd("C:/Users/MMKONDIWA/OneDrive - CIMMYT/Documents/GitHub/Rice_Wheat_System_SDS_Tool")
+# Multivariate geoadditive model
+# remotes::install_git("https://git.uibk.ac.at/c4031039/mvnchol")
+#
+library(mvnchol)
+library(BayesX)
+library(R2BayesX)
+library(sf)
+library(spdep)
+library(rio)
+
+Irrig_Rev_rice_wheat <- import("data/Irrig_Rev_rice_wheat_Updated.csv")
+
+Irrig_Rev_rice_wheat <- subset(Irrig_Rev_rice_wheat, !(is.na(Irrig_Rev_rice_wheat$Longitude)))
+Irrig_Rev_rice_wheat <- subset(Irrig_Rev_rice_wheat, !(is.na(Irrig_Rev_rice_wheat$Latitude)))
+
+library(janitor)
+library(tidyr)
+
+
+Irrig_Rev_rice_wheat$harvest_day_rice <- Irrig_Rev_rice_wheat$l_crop_duration_days_rice + Irrig_Rev_rice_wheat$sowdate_fmt_rice_day
+
+shpname <- file.path(getwd(), "shp", "India_aoi_sf_sp")
+
+India_aoi_sp_bnd <- BayesX::shp2bnd(shpname = shpname, regionnames = "District", check.is.in = F)
+
+K <- neighbormatrix(India_aoi_sp_bnd)
+head(K)
+
+## Also need to transform to factor for
+## setting up the MRF smooth.
+Irrig_Rev_rice_wheat$District <- as.factor(Irrig_Rev_rice_wheat$a_q103_district)
+
+## Now note that not all regions are observed,
+## therefore we need to remove those regions
+## from the penalty matrix
+rn <- rownames(K)
+lv <- levels(Irrig_Rev_rice_wheat$District)
+i <- rn %in% lv
+K <- K[i, i]
+
+set.seed(321)
+library(bamlss)
+
+
 f_wheat_yield_MRF <- list(
   l_ton_per_hectare ~ 1 + variety_type_NMWV + s(sowdate_fmt_wheat_day) + g_q5305_irrig_times + nperha + p2o5perha + s(District, bs = "mrf", xt = list("penalty" = K)) +
     s(District, bs = "re"),
@@ -8,3 +57,10 @@ f_wheat_yield_MRF <- list(
 )
 set.seed(321)
 b_wheat_yield_MRF <- bamlss(f_wheat_yield_MRF, data = Irrig_Rev_rice_wheat, family = "gaussian")
+
+library(distreg.vis)
+library(bamlss)
+
+if (interactive()) {
+  distreg.vis:: vis()
+}
